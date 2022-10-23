2b4269fb2a100c4af36571454942f467ed204b9c
alecw <alecw@users.noreply.github.com>
2022-10-22 22:27:08 -0400

    Fix copyright message

--- src/python/format_court_list.py
+++ src/python/format_court_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # MIT License
 # 
-# Copyright 2022 Broad Institute
+# Copyright 2022 Alec Wysoker
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
