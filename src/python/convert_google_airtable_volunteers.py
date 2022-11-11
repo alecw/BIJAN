15486a326a7ea2d923e507c6a082bd2f8a67c4bc
Alec Wysoker <alecw@broadinstitute.org>
2022-11-10 20:49:37 -0500

    Translate some enumerated columns to different values

--- src/python/convert_google_airtable_volunteers.py
+++ src/python/convert_google_airtable_volunteers.py
@@ -32,12 +32,54 @@ import sys
 import pandas
 import google_sheet_cols
 
+# Some enumerations are different in old and new forms
+weekdays_value_conv = {
+    'only during the summer / solamente durante el verano': "summertime only / solamente durante el verano"
+}
+
+english_value_conv = {
+    'proficient / proficiente' : 'proficient'
+}
+
+portuguese_value_conv = {
+    'fluente / fluent' : 'fluente',
+    'un poco / some' : ' um pouco'
+}
+
+values_conversion_dict = {
+    google_sheet_cols.WEEKDAYS_COL: weekdays_value_conv,
+    google_sheet_cols.ENGLISH_COL: english_value_conv,
+    google_sheet_cols.PORTUGUESE_COL: portuguese_value_conv
+}
+
+def convert_values(df):
+    for row_index in range(len(df)):
+        for col_index, dct in values_conversion_dict.items():
+            if df.iloc[row_index, col_index] in dct:
+                df.iloc[row_index, col_index] = dct[df.iloc[row_index, col_index]]
+    return(df)
+
 def main(args=None):
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("--input", "-i", required=True, help="Input CSV downloaded from Google Sheet 'BIJAN Community Sign-Up (Responses)'.")
     parser.add_argument("--output", "-o", required=True, help="Output CSV to be uploaded to AirTable BIJAN Volunteer Database.")
     options = parser.parse_args(args)
     df = pandas.read_csv(options.input, parse_dates=[0])
+    if False:
+        # experimenting
+        interesting_cols = frozenset(['Daytime during the week / durante el día, los días de semana',
+                                      'English / ingles', 'Portuguese / portugués'
+                                      ])
+        for col in df.columns:
+            if col in interesting_cols:
+                print(f"{col}: {df[col].unique()}")
+    df = convert_values(df)
+    if False:
+        # experimenting
+        print("after")
+        for col in df.columns:
+            if col in interesting_cols:
+                print(f"{col}: {df[col].unique()}")
     out_dict = {
         "First Name": df.iloc[:, google_sheet_cols.FIRSTNAME_COL],
         "Last Name": df.iloc[:, google_sheet_cols.LASTNAME_COL],
